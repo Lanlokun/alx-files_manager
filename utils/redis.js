ff8fb5ae75a3a1dea48bb5333daba60feb753a0f
lanlokun <mlanlokun@gmail.com>
2023-09-04 15:48:06 +0300

    fixed lint errors

--- utils/redis.js
+++ utils/redis.js
@@ -3,34 +3,33 @@ import { promisify } from 'util';
 
 /** redis client */
 
-
 class RedisClient {
-    constructor() {
-        this.client = redis.createClient();
-        this.client.on('error', (error) => {
-            console.log(`Redis client not connected to the server: ${error.message}`);
-        });
-        this.client.on('connect', () => {
-            console.log('Redis client connected to the server');
-        });
-    }
+  constructor() {
+    this.client = redis.createClient();
+    this.client.on('error', (error) => {
+      console.log(`Redis client not connected to the server: ${error.message}`);
+    });
+    this.client.on('connect', () => {
+      console.log('Redis client connected to the server');
+    });
+  }
 
-    isAlive() {
-        return this.client.connected;
-    }
+  isAlive() {
+    return this.client.connected;
+  }
 
-    async get(key) {
-        const getAsync = promisify(this.client.get).bind(this.client);
-        return getAsync(key);
-    }
+  async get(key) {
+    const getAsync = promisify(this.client.get).bind(this.client);
+    return getAsync(key);
+  }
 
-    async set(key, value, duration) {
-        this.client.setex(key, duration, value);
-    }
+  async set(key, value, duration) {
+    this.client.setex(key, duration, value);
+  }
 
-    async del(key) {
-        this.client.del(key);
-    }
+  async del(key) {
+    this.client.del(key);
+  }
 }
 
 const redisClient = new RedisClient();
