1b90df00c0ac346c7769c8a4d27fcf4036810422
lanlokun <mlanlokun@gmail.com>
2023-09-05 13:27:05 +0300

    Create a new user

--- routes/index.js
+++ routes/index.js
@@ -8,6 +8,8 @@ import { APIError, errorResponse } from '../middlewares/error';
 const injectRoutes = (api) => {
   api.get('/status', AppController.getStatus);
   api.get('/stats', AppController.getStats);
+  api.post('/users', AppController.postNew);
+  api.get('/users/me', AppController.getMe);
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
   });
