5d9e84af3b744c4d480ed1d88f8cebf139574623
lanlokun <mlanlokun@gmail.com>
2023-09-05 15:09:11 +0300

    First file

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 import AppController from '../controllers/AppController';
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import FilesController from '../controllers/FilesController';
 import { APIError, errorResponse } from '../middlewares/error';
 import { basicAuthenticate, xTokenAuthenticate } from '../middlewares/auth';
 
@@ -15,6 +16,12 @@ const injectRoutes = (api) => {
   api.get('/connect', basicAuthenticate, AuthController.getConnect);
   api.get('/disconnect', xTokenAuthenticate, AuthController.getDisconnect);
   api.get('/users/me', xTokenAuthenticate, UsersController.getMe);
+  api.post('/files', xTokenAuthenticate, FilesController.postUpload);
+  api.get('/files/:id', xTokenAuthenticate, FilesController.getShow);
+  api.get('/files', xTokenAuthenticate, FilesController.getIndex);
+  api.put('/files/:id/publish', xTokenAuthenticate, FilesController.putPublish);
+  api.put('/files/:id/unpublish', xTokenAuthenticate, FilesController.putUnpublish);
+  api.get('/files/:id/data', xTokenAuthenticate, FilesController.getFile);
   api.all('*', (req, res, next) => {
     errorResponse(new APIError(404, `Cannot ${req.method} ${req.url}`), req, res, next);
   });
